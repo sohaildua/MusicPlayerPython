e8d494633ca7b6a88b24b40f7315b0530174a8bd
sdgamer007 <sohaildua38@example.com>
2018-10-19 18:19:48 +0530

    2nd commit of my code

--- module1.py
+++ module1.py
@@ -7,6 +7,7 @@ mixer.init()
 window.geometry('300x300')
 window.title('My Music Player')
 
+
 textLabel = Label(window,text="This is a Play button");
 textLabel.pack()
 
@@ -23,7 +24,17 @@ def stop_music():
     print("i am being stopped")
     mixer.music.stop()
 
+
 stopphoto = PhotoImage(file="pause-button.png")
 stopbutton = Button(window, image = stopphoto, command = stop_music)
 stopbutton.pack()
+
+def set_value(value):
+    print(value)
+    volume = int(value)/100
+    mixer.music.set_volume(volume)
+
+scale = Scale(window,from_ =0 , to = 100 , orient = HORIZONTAL ,command = set_value)
+scale.set(70)
+scale.pack()
 window.mainloop()
